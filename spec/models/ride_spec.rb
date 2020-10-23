60b7f7cc9e4e8588c761d6ab27c34fdd05871f48
Austin Aspaas <araspaas@live.com>
2020-10-23 10:48:10 -0600

    Add average thrill rating model test

--- spec/models/ride_spec.rb
+++ spec/models/ride_spec.rb
@@ -5,4 +5,15 @@ describe Ride, type: :model do
     it { should belong_to :amusement_park }
     it { should have_many(:mechanics).through(:ride_mechanics) }
   end
+
+  describe "class methods" do
+    it ".average_thrill_rating" do
+      six_flags = AmusementPark.create!(name: "Six Flags Great America", price: 45)
+      american_eagle = six_flags.rides.create!(name: "American Eagle", thrill_rating: 7)
+      raging_bull = six_flags.rides.create!(name: "Raging Bull", thrill_rating: 9)
+      giant_drop = six_flags.rides.create!(name: "Giant Drop", thrill_rating: 10)
+
+      expect(six_flags.rides.average_thrill_rating.round(1)).to eq(8.7)
+    end
+  end
 end
