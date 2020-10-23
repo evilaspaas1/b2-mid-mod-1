ab549bd1a1107258ed1faf447e6a33143c2f4f7b
Austin Aspaas <araspaas@live.com>
2020-10-23 11:36:30 -0600

    Add routes for mechanic show page and creating work for mechanic

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get '/mechanics', to: 'mechanics#index'
+  get '/mechanics/:id', to: 'mechanics#show'
 
   get '/amusement_parks/:id', to: 'amusement_parks#show'
+  post '/ride_mechanics/:mechanic_id', to: 'ride_mechanics#create'
 end
