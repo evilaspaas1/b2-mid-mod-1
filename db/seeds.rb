c5338c44fbe99186b1f39e0803b9f6a2ea7e9970
Austin Aspaas <araspaas@live.com>
2020-10-23 11:47:22 -0600

    Add seeds to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+van = Mechanic.create!(name: "Van Stone-arm", experience: 7)
+old_greg = Mechanic.create!(name: "Greg", experience: 35)
+
+six_flags = AmusementPark.create!(name: "Six Flags Great America", price: 45)
+
+american_eagle = six_flags.rides.create!(name: "American Eagle", thrill_rating: 7)
+raging_bull = six_flags.rides.create!(name: "Raging Bull", thrill_rating: 9)
+giant_drop = six_flags.rides.create!(name: "Giant Drop", thrill_rating: 10)
+v2 = six_flags.rides.create!(name: "V2", thrill_rating: 7)
+
+RideMechanic.create!(mechanic_id: van.id, ride_id: american_eagle.id)
+RideMechanic.create!(mechanic_id: van.id, ride_id: raging_bull.id)
+RideMechanic.create!(mechanic_id: van.id, ride_id: giant_drop.id)
