a30e00245b50a6f192f8ddf490673b7e87ec3e28
Austin Aspaas <araspaas@live.com>
2020-10-23 09:58:31 -0600

    Add a migration and models with associations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_23_154141) do
+ActiveRecord::Schema.define(version: 2020_10_23_154310) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -78,6 +78,15 @@ ActiveRecord::Schema.define(version: 2020_10_23_154141) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "ride_mechanics", force: :cascade do |t|
+    t.bigint "ride_id"
+    t.bigint "mechanic_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["mechanic_id"], name: "index_ride_mechanics_on_mechanic_id"
+    t.index ["ride_id"], name: "index_ride_mechanics_on_ride_id"
+  end
+
   create_table "rides", force: :cascade do |t|
     t.string "name"
     t.integer "thrill_rating"
@@ -99,5 +108,7 @@ ActiveRecord::Schema.define(version: 2020_10_23_154141) do
   add_foreign_key "flight_passengers", "flights"
   add_foreign_key "flight_passengers", "passengers"
   add_foreign_key "flights", "airlines"
+  add_foreign_key "ride_mechanics", "mechanics"
+  add_foreign_key "ride_mechanics", "rides"
   add_foreign_key "rides", "amusement_parks"
 end
